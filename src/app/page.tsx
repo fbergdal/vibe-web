e3100d7cf203758a0eabaff85e42b9881e9da80e
fbergdal <fbergdal@mac.com>
2025-09-04 16:23:52 +0200

    New logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
       <Image 
-        src="/next.svg" 
+        src="/vibe_logo.png" 
         alt="Vibe Logo" 
         width={400} 
         height={400} 
